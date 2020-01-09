ce3d5f85cd3f0a6b40866b6f524d5166052df7d8
CUSTzt <45364609+CUSTzt@users.noreply.github.com>
2020-01-09 19:48:02 +0800

    test

--- icpc/2015长春/F.cpp
+++ icpc/2015长春/F.cpp
@@ -1,43 +1,32 @@
- /****************************************************************
- # @Author:      miniLCT
- # @DateTime: 2019年12月30日 星期一 16时21分19秒
- # @Description: You build it. You run it 
- # @More: Once lots of AC, try Brute-force,dynamic programming 
- ****************************************************************/
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
-#define close std::ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
-#define eps 1e-8
-typedef long long ll;
-const int maxn = 1e6;
-const int INF = 1e9;
-const ll linf = 0x3f3f3f3f3f3f3f3f;
-const double PI = acos(-1.0);
-ll mod = 1e9+7;
-int a[maxn];
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        int p = 0, pp = 0;
-        for(int i = 0; i < n;i++){
-            scanf("%d", &a[i]);
-            if(i >= 1){
-                if(a[i] > a[i-1])p++;
-            }
-        }
-            if(p <= 1)puts("YES");
-            else puts("NO"); 
-    }
-    return 0;
+int n,len;
+int dp[100005];
+int a[100005],b[100005];
+int solve(int a[])
+{
+	int len;
+	dp[0]=a[0];
+	len=1;
+	for(int i=1;i<n;i++)
+	{
+		dp[a[i] >= dp[len - 1] ? len++ : upper_bound(dp, dp+ len, a[i]) - dp] = a[i];
+	}
+	return len;
 }
-
-/*****************************************************************
- stuff you should look for
-  * int overflow, array bounds
-  * special cases (n=1?), set tle
-  * do smth instead of nothing and stay organized
-*****************************************************************/
+int main()
+{
+	int t;
+	scanf("%d",&t);
+	while(t--){
+		scanf("%d",&n);
+		int flag=n-1;
+		for(int i=0;i<n;i++){
+			scanf("%d",&a[i]);
+			b[flag--]=a[i];
+		}
+		int sum1=solve(a),sum2=solve(b);
+		if((sum1>=n-1)||(sum2>=n-1)) printf("YES\n");
+		else printf("NO\n");
+	}
+ } 
--- math/多项式/拉格朗日插值/luoguo4781拉格朗日插值模板.cpp
+++ math/多项式/拉格朗日插值/luoguo4781拉格朗日插值模板.cpp
@@ -1,28 +1,55 @@
- /************************************************
- # @Author:      miniLCT
- # @DateTime:    2019-11-30 12:23:22
- # @Description: You build it.You run it
- # @More: If lots of AC,try BF,dfs,DP
- ***********************************************/
-#include <bits/stdc++.h>
-
+/****************************************************************
+# @Author:      iLovePKU_zbtxdy
+# @DateTime:    2020-01-09 16:34:40
+# @Description: You build it. You run it 
+# @More: Once lots of AC, try Brute-force,dynamic programming 
+****************************************************************/
+#include<bits/stdc++.h>
 using namespace std;
+#define close std::ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
 #define eps 1e-8
-#define close ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 typedef long long ll;
 const int maxn = 1e6;
 const int INF = 1e9;
 const ll linf = 0x3f3f3f3f3f3f3f3f;
 const double PI = acos(-1.0);
-ll mod = 1e9+7;
+ll mod = 998244353;
+using LL = long long ;
+LL bin(LL x, LL n, LL MOD) {
+    LL ret = MOD != 1;
+    for (x %= MOD; n; n >>= 1, x = x * x % MOD)
+        if (n & 1) ret = ret * x % MOD;
+    return ret;
+}
+ll x[maxn], y[maxn], n , k;
 int main()
 {
-
+	close;
+	cin >> n >> k;
+	for(int i = 1;i <= n ; i++){
+		cin >> x[i] >> y[i];
+	}
+	int ans = 0, t = 0;
+	for(int i = 1; i <= n; i++){
+		t = 1;
+		for(int j = 1; j <= n; j++)
+			if(i != j){
+				t = t * (x[i] + mod - x[j]) % mod;
+			}
+			t = bin( t , mod - 2, mod);
+		for(int j = 1; j <= n; j++)
+			if(i != j){
+				t = t*(k + mod -x[j]) % mod; 
+			}
+		    t = t*y[i] % mod;
+		    ans = ans + t ;
+		    ans %= mod;
+	}
+	cout << ans << endl;
 }
-
-/******************************************************
- stuff you should look for
-  * int overflow, array bounds
-  * special cases (n=1?), set tle
-  * do smth instead of nothing and stay organized
-*******************************************************/
+/***************************************************************************
+ *stuff you should look for
+ *int overflow, array bounds
+ *special cases (n=1?), set tle
+ *do smth instead of nothing and stay organized
+***************************************************************************/
